787c673cfd09801cec84e00c23e87daeef6ae737
EddyBautista <bautista.j.eddy@gmail.com>
2021-09-06 06:26:44 -0500

    set up about controller with read/write functionality

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -6,6 +6,11 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using net_core_app.Models;
+using Firebase.Database;
+using Firebase.Database.Query;
+using System.Linq;
+using System.Threading.Tasks;
+
 
 namespace net_core_app.Controllers
 {
@@ -32,6 +37,40 @@ namespace net_core_app.Controllers
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        public async Task<ActionResult> About()
+        {
+            // simulate test user data and login timestamp
+            var userId = "12345";
+            var currentLoginTime = DateTime.Utc.ToString("MM/dd/yyyy HH:mm:ss");
+
+            // save non identifying data to firebase
+            var currentUserLogin = new LoginData() { TimeStampUtc = currentLoginTime};
+            var firebaseClient = new firebaseClient("");
+            var result = await firebaseClient
+                .Child("Users/" + userID + "/Logins")
+                .PostAsync(currentUserLogin);
+
+            // grab data from firebase 
+            var dbLogins = await firebaseClient
+                .Child("Users")
+                .Child(userID)
+                .Child("Logins")
+                .OnceAsync<LoginData>();
+
+            var timestampList = new List<DateTime>();
+
+            //Convert JSON data to original datatype
+            foreach (var login in dbLogins)
+            {
+                timestampList.Add(Convert.ToDateTime(login.Object.TimestampUtc).ToLocalTime());
+            }
+            //Pass data to the view
+            ViewBag.CurrentUser = userId;
+            ViewBag.Logins = timestampList.OrderByDescending(x => x);
+            return View();
+
         }
     }
 }
